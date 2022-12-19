1238d0688e031c83f661f98d567743c1592020c9
MOHIT PIPALIYA <pipaliyamohit4g@gmail.com>
2022-12-20 01:21:55 +0530

    First initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+  $("#box").append(
+    '<div class="card"><h2>Accenture - William D. Green</h2><hr /><div class="imgs"><img src="img/William D. Green.jpg" alt="" class="img" /><img src="img/accenture.png" alt="" class="img" /></div></div>'
+  );
+});
